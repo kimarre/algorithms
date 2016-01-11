2169e96c95d96a217e533ac7f5982f2a7c3838d4
kimarre <kimarrexo@gmail.com>
2016-01-10 20:54:52 -0800

    Add first assignment dir

---
+++ assignment1/SelectionSort.java
@@ -0,0 +1,19 @@
+
+/**
+ * @author Kim Arre
+ * Winter 2016
+ * CPE 349 - Theresa Migler-VonDollen
+ */
+
+public class SelectionSort {
+   /**
+    * This is just for funsies, because C<->Java conversions are hard. :)
+    */
+   public static void printf(String msg) {
+      System.out.println(msg);
+   }
+
+   public static void main(String[] args) {
+      printf("Hello, world!");
+   }
+}
